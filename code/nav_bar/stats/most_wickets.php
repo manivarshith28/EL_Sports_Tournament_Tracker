6ce74585500f530ab856f8204b477a16d87296bb
manivarshith28 <manivarshith2806@gmail.com>
2025-07-25 16:18:20 +0530

    Create most_wickets.php

---
+++ code/nav_bar/stats/most_wickets.php
@@ -0,0 +1,39 @@
+<?php
+    include '../../../db.php';
+
+    $sql = "SELECT * FROM most_wickets LIMIT 25";
+    $result = $conn->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Most Wickets</title>
+    <link rel="stylesheet" type="text/css" href="../../../style.css">
+</head>
+<body>
+    <h2 style="text-align: center;">MOST WICKETS</h2>
+    <?php
+        if ($result && $result->num_rows > 0) {
+            echo "<table>";
+            echo "<tr>";
+            while ($fieldinfo = $result->fetch_field()) {
+                echo "<th>" . htmlspecialchars($fieldinfo->name) . "</th>";
+            }
+            echo "</tr>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                foreach ($row as $data) {
+                    echo "<td>" . htmlspecialchars($data) . "</td>";
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "<p style='text-align: center;'>No data available</p>";
+        }
+        $conn->close();
+    ?>
+</body>
+</html>
